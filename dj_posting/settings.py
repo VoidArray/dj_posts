949ae222540433424924c46a7f1c0937493f6881
Anton Soloninin <voidarray@gmail.com>
2017-02-09 18:13:32 +0500

    Switch to mysql

--- dj_posting/settings.py
+++ dj_posting/settings.py
@@ -1,5 +1,7 @@
 import os
 
+from .settings_local import DATABASES
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -60,16 +62,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'dj_posting.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 CACHES = {
     "default": {
